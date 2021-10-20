196273d78fc7d12b2ebd171daf31c980a6bb040d
Stefan Krastanov <stefan@krastanov.org>
2021-10-20 16:30:51 +0000

    Keep upcoming talk visible for one more day.

--- briefings_server.py
+++ briefings_server.py
@@ -129,14 +129,16 @@ class Root:
     def index(self):
         with conn() as c:
             all_talks = list(c.execute('SELECT date, speaker, affiliation, title, abstract, bio, conf_link FROM events WHERE warmup=0 ORDER BY date ASC'))
-        records = [t for t in all_talks if t[0]>datetime.datetime.now()]
+        now = datetime.datetime.now() - datetime.timedelta(days=1)
+        records = [t for t in all_talks if t[0]>now]
         return templates.get_template('__index.html').render(records=records, calendarframe=conf('google.calendariframe'), banner=conf('frontpage.banner'), customfooter=conf('frontpage.footer'), ical=conf('google.calendarical'))
 
     @cherrypy.expose
     def iframeupcoming(self):
         with conn() as c:
             all_talks = list(c.execute('SELECT date, speaker, affiliation, title, abstract, bio, conf_link FROM events WHERE warmup=0 ORDER BY date ASC'))
-        records = [t for t in all_talks if t[0]>datetime.datetime.now()]
+        now = datetime.datetime.now() - datetime.timedelta(days=1)
+        records = [t for t in all_talks if t[0]>now]
         return templates.get_template('__iframeupcoming.html').render(records=records)
 
     @cherrypy.expose
