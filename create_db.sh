a5ed6883753f2a65637ef99f040336577cb5fad0
Stefan Krastanov <stefan@krastanov.org>
2023-01-08 16:19:04 -0500

    fix a column type in the database

--- create_db.sh
+++ create_db.sh
@@ -19,7 +19,7 @@ CREATE TABLE events
  host TEXT,
  host_email TEXT,
  location TEXT,
- announced BOOLEAN DEFAULT 0,
+ announced INT DEFAULT 0,
  recording_processed BOOLEAN DEFAULT 0,
  PRIMARY KEY (date, warmup)
 );
