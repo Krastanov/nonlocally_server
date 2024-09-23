cb890a9dbadc5a4de6e8e34a84cfd9aec8fe9c4b
Stefan Krastanov <stefan@krastanov.org>
2024-09-23 00:19:42 -0400

    print status messages in new_pad.py

--- new_pad.py
+++ new_pad.py
@@ -1,8 +1,11 @@
 from briefings_server import *
+print("Started")
 etherpad = py_etherpad.EtherpadLiteClient(apiKey=conf("etherpad.apikey"),baseUrl=conf("etherpad.url")+'/api')
+print("Etherpad loaded")
 prefix = SEMINAR_SERIES 
 padid = (prefix+str(uuid.uuid4())).replace('-','')
 #etherpad.copyPad(conf('etherpad.scheduletemplate'), padid)
+print("Creating pad...")
 etherpad.createPad(padid)
 print("Created pad:")
 print(padid)
